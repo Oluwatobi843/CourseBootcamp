bdbb7046fa463be968ec564abd9d550c9a3b8b22
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-08-08 06:59:10 +0100

    Update bootcamps.js

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,10 +5,84 @@ const Bootcamp = require('../models/Bootcamp');
 const qs = require('qs');
 
 
+// // @desc    Get all bootcamps
+// // @route   GET /api/v1/bootcamps
+// // @access  Public
+// exports.getBootcamps = asyncHandler(async (req, res, next) => {
+//   const removeFields = ["select"];
+//   const parsedQuery = qs.parse(req._parsedUrl.query);
+
+//   // Remove unwanted fields
+//   removeFields.forEach((param) => delete parsedQuery[param]);
+
+//   // Convert string booleans to actual booleans
+//   const convertBooleans = (obj) => {
+//     for (const key in obj) {
+//       if (typeof obj[key] === "object") {
+//         convertBooleans(obj[key]);
+//       } else {
+//         if (obj[key] === "true") obj[key] = true;
+//         if (obj[key] === "false") obj[key] = false;
+//       }
+//     }
+//   };
+//   convertBooleans(parsedQuery);
+
+//   // Expand dot notation into nested objects
+//   const expandedQuery = expandDotNotation(parsedQuery);
+
+//   // Convert MongoDB operators
+//   let queryStr = JSON.stringify(expandedQuery);
+//   queryStr = queryStr.replace(
+//     /\b(gt|gte|lt|lte|in)\b/g,
+//     (match) => `$${match}`
+//   );
+//   const mongoQuery = JSON.parse(queryStr);
+
+//   // Log for debugging
+//   console.log("Parsed Query:", parsedQuery);
+//   console.log("Final Mongo Query:", mongoQuery);
+
+//   // Execute query
+//   const bootcamps = await Bootcamp.find(mongoQuery);
+//   console.log("Matched Bootcamps:", bootcamps);
+
+//   res.status(200).json({
+//     success: true,
+//     count: bootcamps.length,
+//     data: bootcamps,
+//   });
+// });
+
+// // Helper function to expand dot-notation keys
+// function expandDotNotation(obj) {
+//   const result = {};
+//   for (const key in obj) {
+//     if (key.includes(".")) {
+//       const parts = key.split(".");
+//       let nested = result;
+//       for (let i = 0; i < parts.length; i++) {
+//         if (!nested[parts[i]]) nested[parts[i]] = {};
+//         if (i === parts.length - 1) {
+//           nested[parts[i]] = obj[key];
+//         } else {
+//           nested = nested[parts[i]];
+//         }
+//       }
+//     } else {
+//       result[key] = obj[key];
+//     }
+//   }
+//   return result;
+// }
+ 
+// Testing
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
+
+  
   const removeFields = ["select"];
   const parsedQuery = qs.parse(req._parsedUrl.query);
 
@@ -53,7 +127,6 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     data: bootcamps,
   });
 });
-
 
 
 
