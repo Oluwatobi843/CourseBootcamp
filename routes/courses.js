a389e6dab73b2e45dfb3a15cc82c5bc7cf23aba2
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-08-09 17:56:38 +0100

    update on course controller, course models and course routes

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,9 @@ const express = require('express');
 const {
     getCourses,
     getCourse,
-    addCourse
+    addCourse,
+    updateCourse,
+    deleteCourse
 } = require("../controllers/courses");
 
 const router = express.Router({ mergeParams: true });
@@ -12,7 +14,10 @@ router.route('/')
        .get(getCourses)
        .post(addCourse) 
 
-router.route('/:id').get(getCourse);
+router.route('/:id')
+       .get(getCourse)
+       .put(updateCourse)
+       .delete(deleteCourse)
 
 
 module.exports = router;
