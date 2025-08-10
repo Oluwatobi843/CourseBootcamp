80260a2f5fcc4279c964fa05b58de232da3ef0fc
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-08-10 07:20:17 +0100

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,4 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const geocoder = require("../utils/geocoder");
+const Bootcamp = require("../models/Bootcamp");
