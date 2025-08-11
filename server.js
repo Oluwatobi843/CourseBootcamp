b789721881c60417a0ab65c97c4b27bc69f46c0a
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-08-11 12:32:12 +0100

    Complete set-up for registration auth

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ connectDB();
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
-const courses = require("./routes/courses")
+const courses = require("./routes/courses");
+const auth = require('./routes/auth')
 
 const app = express();
 
@@ -35,7 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
-app.use("/api/v1/courses", courses)
+app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 // Error Handler
 app.use(errorHandler); 
