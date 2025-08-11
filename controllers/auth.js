b613317707dde229655e7517b48fab301dd4bd05
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-08-11 16:09:45 +0100

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,7 +29,7 @@ exports.register = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/auth/login
 // @access  Public
 
-exports.register = asyncHandler(async (req, res, next) => {
+exports.login = asyncHandler(async (req, res, next) => {
 
   const { email, password} = req.body;
 
@@ -40,7 +40,7 @@ if (!email || !password ){
 }
 // check for user
 
-const user = await user.findOne({ email}).select('+password');
+const user = await User.findOne({ email}).select('+password');
 
 if(!user){
    return next(new ErrorResponse("Invalid Credentials", 401));
@@ -54,8 +54,30 @@ if(!isMatch){
    return next(new ErrorResponse("Invalid Credentials", 401));
 }
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token: token})
+sendTokenResponse(user, 200, res);
 }) 
+
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token 
+
+  const token  = user.getSignedJwtToken();
+
+  const options = {
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000 ),
+      httpOnly: true
+  };
+
+  if(process.env.NODE_ENV === 'production'){
+    options.secure = true;
+  }
+
+  res
+      .status(statusCode)
+      .cookie('token', token, options)
+      .json({
+        success: true, 
+        token
+      });
+}
