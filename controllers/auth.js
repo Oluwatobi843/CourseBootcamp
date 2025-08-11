564ac81c165343e82ba29a3308c32f304747d9de
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-08-11 12:04:26 +0100

    Creation of auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+// const geocoder = require("../utils/geocoder");
+const User = require("../models/User");
+
+
+
+
--- routes/auth.js
+++
@@ -1,4 +0,0 @@
-const ErrorResponse = require("../utils/errorResponse");
-const asyncHandler = require("../middleware/async");
-const geocoder = require("../utils/geocoder");
-const Bootcamp = require("../models/Bootcamp");
