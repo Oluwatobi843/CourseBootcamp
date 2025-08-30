45c8582993f0a182b48025e37e2de13c11cd5681
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-08-30 10:51:59 +0100

    update on forgotpassword

--- models/User.js
+++ models/User.js
@@ -63,16 +63,16 @@ UserSchema.methods.getResetPasswordToken = function () {
   // Generate token
   const resetToken = crypto.randomBytes(20).toString("hex");
 
-//   // Hash token and set to resetPasswordToken field
-//   this.resetPasswordToken = crypto
-//     .createHash("sha256")
-//     .update(resetToken)
-//     .digest("hex");
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
-//   // Set expire
-//   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
 
-//   return resetToken;
-// };
+  return resetToken;
+};
 
 module.exports = mongoose.model("User", UserSchema);
