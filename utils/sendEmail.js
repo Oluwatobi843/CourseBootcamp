c0b88daf7b0e16bf5e731f4c3a2285db23600e05
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-08-31 06:51:27 +0100

    Update on controller auth.js and sendEmail utils

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -18,5 +18,8 @@ const sendEmail = async (options)  => {
     text: options.message
   }
 
-  
+  const info = await transporter.sendMail(message);
+  console.log('Message sent: %s', info.messageId);
 }
+
+module.exports = sendEmail;
