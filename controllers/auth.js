75319d1b89852a89116bcc41cc959d26c5e811d9
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-09-06 09:57:39 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -90,7 +90,7 @@ exports.forgotPassword = asyncHandler (async (req, res, next) =>{
     await user.save({ validateBeforeSave: false })
 
     // Create reset url
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
 
     const message = `You are receiving this email because you(or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
 
@@ -122,12 +122,11 @@ exports.forgotPassword = asyncHandler (async (req, res, next) =>{
 // @desc    Reset password
 // @route   PUT /api/v1/auth/resetpassword/:resettoken
 // @access  Public
-
 exports.resetPassword = asyncHandler(async (req, res, next) => {
   // Get hashed token
   const resetPasswordToken = crypto
     .createHash("sha256")
-    .update(req.params.resettokken)
+    .update(req.params.resettoken) // ✅ fixed typo here
     .digest("hex");
 
   const user = await User.findOne({
@@ -139,14 +138,15 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse("Invalid token", 400));
   }
 
-  // set new password
+  // Set new password
   user.password = req.body.password;
   user.resetPasswordToken = undefined;
   user.resetPasswordExpire = undefined;
   await user.save();
 
   sendTokenResponse(user, 200, res);
-});   
+});
+   
           
 
 // Get token from model, create cookie and send response
