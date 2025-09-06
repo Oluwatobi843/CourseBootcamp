0f295d7308320bf2fe73bdbdc236d2db544dfd77
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-09-06 14:40:47 +0100

    Update

---
+++ routes/reviews.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const {
+  getReviews
+} = require("../controllers/reviews");
+
+const Review = require("../models/Review");
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
+
+router
+  .route("/")
+  .get(
+    advancedResults(Course, {
+      path: "bootcamp",
+      select: "name description",
+    }),
+    getCourses
+  )
+  .post(protect, authorize("publisher", "admin"), addCourse);
+
+router
+  .route("/:id")
+  .get(getCourse)
+  .put(protect, authorize("publisher", "admin"), updateCourse)
+  .delete(protect, authorize("publisher", "admin"), deleteCourse);
+
+module.exports = router;
