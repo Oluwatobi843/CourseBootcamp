31680da5a13d41248fc61ec9c41e501d1f9b7dc0
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-09-12 12:57:35 +0100

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -38,6 +38,8 @@ const ReviewSchema = new mongoose.Schema({
       required: true
     }
      
+      
+
     
   });
    
@@ -45,5 +47,4 @@ const ReviewSchema = new mongoose.Schema({
   // Prevent User from submiting more than one review per bootcamp
   ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
-  module.exports = mongoose.model('Review', ReviewSchema);
-  
+  module.exports = mongoose.model('Review', ReviewSche
