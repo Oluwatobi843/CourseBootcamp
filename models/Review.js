9466308609bb3f4161baa84851d04d97ba0b25c2
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-09-15 08:45:27 +0100

    Update

--- models/Review.js
+++ models/Review.js
@@ -44,5 +44,44 @@ const ReviewSchema = new mongoose.Schema({
   // Prevent User from submiting more than one review per bootcamp
   ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+  
+  // static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function(bootcampId){
+  // console.log('Calculating avg Cost...'.blue)
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp : bootcampId}
+    },
+    {
+      $group: { 
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' }
+        
+      }
+    }
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating
+    })
+  } catch (err) {
+      console.error(err);
+  }
+}
+
+
+
+  //  Call getAverageCost after save
+  ReviewSchema.post('save', function(){
+      this.constructor.getAverageRating(this.bootcamp);
+  }) 
+
+  // Call getAverageCost before remove
+  ReviewSchema.pre('remove', function(){
+     this.constructor.getAverageRating(this.bootcamp);
+  }) 
+
   module.exports = mongoose.model('Review', ReviewSchema);
   
