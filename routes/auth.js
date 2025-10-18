91a106a90701fb1ef7f76214c71c5760af0bdce3
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-10-18 21:12:46 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -21,5 +21,8 @@ router.put('/updatedetails',protect, updateDetails);
 router.put('/updatepassword',protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
+
+
+
             
 module.exports = router;
