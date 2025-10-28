039327596e1d9a2ecea367cc323d5c53edda2ecd
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-10-28 19:33:47 +0100

    update

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require("express-mongo-sanitize"); 
 const helmet = require("helmet"); 
 const xss = require("xss-clean"); 
+const rateLimit = require("express-rate-limit"); 
+const hpp = require("hpp"); 
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 
 // Load env vars
@@ -49,10 +52,25 @@ app.use(helmet());
 // Prevent XSS attacks
 app.use(xss());
 
+// Rate Limiting
+ const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100     
+ });
+
+ app.use(limiter);
+
+
+//  Prevent http param pollution
+app.use(hpp());
+
+// Enables CORS
+app.use(cors())
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Mount routers
+// Mount routers  
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
 app.use("/api/v1/auth", auth);
